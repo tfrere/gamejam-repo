329075ed1c384d07f79e5c7aafcd2343bc3125f7
arnaud brun <arnaud.brun.vidal@gmail.com>
2021-05-01 20:36:29 +0200

    Arnaud - BaseMovement script with default shaolin sprite

--- Assets/Scenes/Arnaud/Scripts/BaseMovement.cs
+++ Assets/Scenes/Arnaud/Scripts/BaseMovement.cs
@@ -2,46 +2,99 @@ using UnityEngine;
 
 public class BaseMovement : MonoBehaviour
 {
-  private float m_speed = 10f;
-  private int numberOfJumpsBeforeTouchingGround = 0;
-  private Rigidbody2D rbody;
-  public string leftInput;
-  public string rightInput;
-  public string topInput;
-  public string bottomInput;
 
-  void Start()
-  {
-      rbody = GetComponent<Rigidbody2D>();
-      print("player start");
-  }
+    // Public configuration
+    // INPUTS
+    public string leftInput = "q";
+    public string rightInput = "d";
+    public string topInput = "z";
+    public string bottomInput = "s";
 
- void OnCollisionEnter2D(Collision2D col) {
-     print("toto");
-     numberOfJumpsBeforeTouchingGround = 0;
- }
+    public float m_speed = 5f;
+    public float m_jump = 10.0f;
+    public float m_fall = 20.0f;
 
- // Update is called once per frame
- void Update()
- {
-     if (Input.GetKey(leftInput))
-     {
-         print("left");
-         rbody.velocity = new Vector2(-m_speed, 0);
-     }
-     if(Input.GetKey(rightInput)) {
-         print("right");
-         rbody.velocity = new Vector2(m_speed, 0);
-     }
-     if (Input.GetKey(topInput) && numberOfJumpsBeforeTouchingGround < 2)
-     {
-         print("up");
-         numberOfJumpsBeforeTouchingGround++;
-         rbody.velocity = new Vector2(0, m_speed);
-     }
-     if(Input.GetKey(bottomInput)) {
-         print("down");
-         rbody.velocity = new Vector2(0, -m_speed);
-     }
- }
+    public string[] collisionTags = { "Wall", "Ground" };
+
+    // GameObject Internals
+    private Rigidbody2D rbody;
+    private SpriteRenderer spriteRenderer;
+
+    
+
+
+    // PRIVATE COOKING
+    private bool facingLeft = false;
+    private bool isGrounded = false;
+    private bool isOnWall = false;
+    private float _defaultMass;
+
+
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _defaultMass = rbody.mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float _prevX = rbody.velocity.x;
+        float _prevY = rbody.velocity.y;
+
+        if (Input.GetKey(leftInput))
+        {
+            rbody.velocity = new Vector2(-m_speed, _prevY);
+            facingLeft = true;
+        }
+        if (Input.GetKey(rightInput))
+        { 
+            rbody.velocity = new Vector2(m_speed, _prevY);
+            facingLeft = false;
+        }
+        if (Input.GetKey(topInput) && (isGrounded || isOnWall))
+        {
+            rbody.velocity = new Vector2(_prevX, m_jump);
+
+        }
+        if (Input.GetKey(bottomInput))
+        {
+            rbody.velocity = new Vector2(_prevX, -m_fall);
+        }
+
+
+        // Sprite direction
+        spriteRenderer.flipX = facingLeft;
+
+        // Rigidbody update
+        if (isOnWall || isGrounded)
+        {
+            rbody.mass = 0.5f * _defaultMass;
+        } else
+        {
+            rbody.mass = _defaultMass;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("OnCollisionEnter2D - " + collision.gameObject.tag);
+        // Enter -> So if tag is present, that's good
+        isGrounded = collision.gameObject.tag == "Ground";
+        isOnWall = collision.gameObject.tag == "Wall";
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        print("OnCollisionExit2D - " + collision.gameObject.tag);
+        // Exit -> So if value was previsouly set and current collision tag if present, reset the value, else keep it like it is
+        isGrounded = isGrounded && collision.gameObject.tag == "Ground" ? false : isGrounded;
+        isOnWall = isOnWall && collision.gameObject.tag == "Wall" ? false : isOnWall;
+    }
+
+
 }
