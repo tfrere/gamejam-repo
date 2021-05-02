33f0c1ef207c0bcd9f17d570d3b4b5c9e0262106
Aymeric <georgin.aymeric@gmail.com>
2021-05-02 16:32:21 +0200

    <Merge> Conflict

---
+++ Assets/Scenes/Aymeric/Scripts/PunchAction.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+
+public class PunchAction : MonoBehaviour
+{
+
+
+    // animator
+    // public Animator animator;
+
+    // spriterenderer
+    // public SpriteRenderer spriteRenderer;
+
+    
+    // Destroy(this.gameObject)
+    // Start is called before the first frame update
+    void Start()
+    {
+        launchPunch();
+    }
+
+    void Update() 
+    {
+        this.transform.position = new Vector2(this.transform.position.x + this.transform.localScale.x * 0.5f, this.transform.position.y); 
+    }
+    
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("DownKick  Player");
+        }
+
+        if (collision.gameObject.tag == "Ground") {
+            Debug.Log("DownKick Ground");
+        }
+    }
+
+    IEnumerator launchPunch() 
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+
+}
