bffc1a38994ea02d23f43c38def9bf30d618f9fb
arnaud brun <arnaud.brun.vidal@gmail.com>
2021-05-10 09:14:40 +0200

    Arnaud - Draft Bow reworked

--- Assets/Experiments/Arnaud/Scripts/__Bow.cs
+++ Assets/Experiments/Arnaud/Scripts/__Bow.cs
@@ -7,40 +7,6 @@ public interface IBowAction
     public void OnThrow(InputValue value);
 }
 
-public class BowActionHandler: MonoBehaviour, IBowAction
-{
-    // Direction
-
-
-    // Stock management
-    private ArrowsStock arrowStock;
-
-    public void AddRefToStock(ArrowsStock s)
-    {
-        arrowStock = s;
-    }
-
-    
-
-    public void OnThrow(InputValue value)
-    {
-        print("Onthrow" + value.Get<float>());
-
-        // Spaw an arrow, if there is still one
-        if (arrowStock.IsEmpty())
-        {
-            print("Cant throw anymore");
-            
-        } else
-        {
-
-            print("Should throw something");
-            arrowStock.Decrease();
-        }
-    }
-
-}
-
 
 public class ArrowsStock
 {
@@ -68,27 +34,72 @@ public class ArrowsStock
 }
 
 
-public class __Bow : MonoBehaviour
+public class __Bow : MonoBehaviour, IBowAction
 {
+
     public int maxArrows = 3;
+    public float arrowSpeed = 2f;
+    public GameObject Arrow;
 
-    private ArrowsStock stock;
+    private Rigidbody2D rb2D;
+    private ArrowsStock arrowStock;
+    private Vector2 direction = Vector2.zero;
 
-    private BowActionHandler bah;
+    private string ARROW_NAME_PREFIX = "ARROW";
 
     void Start()
     {
-        
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
         // Initialize stock of arrows
-        stock = new ArrowsStock(maxArrows);
-        // Initialize action handler
-        bah = gameObject.AddComponent<BowActionHandler>();
-        bah.AddRefToStock(stock);
+        arrowStock = new ArrowsStock(maxArrows);
     }
 
     void Update()
     {
-        
-        
+        direction = rb2D.GetPointVelocity(rb2D.position);
+        //print("Direction " + direction);
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.StartsWith(ARROW_NAME_PREFIX))
+        {
+            print("current velocity of arrow : " + collision.rigidbody.velocity.magnitude);
+            print("RelativeVelocity - " + collision.relativeVelocity);
+            arrowStock.Increase();
+        }
+    }
+
+
+
+
+
+    public void OnThrow(InputValue value)
+    {
+        // Spaw an arrow, if there is still one
+        if (arrowStock.IsEmpty())
+        {
+            print("Cant throw anymore");
+        }
+        else
+        {
+            bool isFacingLeft = direction.x < 0;
+
+            float positionOffset = isFacingLeft ? -1 : 1f;
+
+
+            Vector3 arrowVector = isFacingLeft ? Vector3.left : Vector3.right;
+            Vector3 arrowPosition = new Vector3(this.transform.position.x + positionOffset, this.transform.position.y, this.transform.position.z);
+            Quaternion quaternion = isFacingLeft ? Quaternion.AngleAxis(0, Vector3.back) : Quaternion.AngleAxis(0, Vector3.forward);
+
+
+            GameObject newArrow = Instantiate(Arrow, arrowPosition, quaternion);
+            newArrow.name = ARROW_NAME_PREFIX;
+            newArrow.GetComponent<Rigidbody2D>().AddForce(arrowVector * arrowSpeed, ForceMode2D.Impulse);
+
+
+            arrowStock.Decrease();
+        }
     }
 }
